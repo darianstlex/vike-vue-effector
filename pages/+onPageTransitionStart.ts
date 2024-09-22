a466d3f8bfcb37a895fcf0d382a2d1f08c723f1e
Ivan Uzun <ivan@motorway.co.uk>
2024-09-22 10:17:58 +0100

    chore: deps, transition

--- pages/+onPageTransitionStart.ts
+++ pages/+onPageTransitionStart.ts
@@ -1,5 +1,7 @@
 import type { OnPageTransitionStartAsync } from 'vike/types';
 
-export const onPageTransitionStart: OnPageTransitionStartAsync = async () => {
-  // document.querySelector('body')?.classList.add('page-is-transitioning');
+export const onPageTransitionStart: OnPageTransitionStartAsync = async (pageContext) => {
+  if (pageContext.isHydration) {
+    document.querySelector('body')?.classList.add('page-is-transitioning');
+  }
 };
