7bbffee8f05f4a861aaafca693ed4218c4c5192e
Ivan Uzun <ivan@motorway.co.uk>
2024-10-04 11:52:22 +0100

    chore: clen up

--- utils/effector/index.ts
+++ utils/effector/index.ts
@@ -1,6 +1,3 @@
-import { getScope } from '@utils/effector/scope';
-
 export { createPageInit, createPageStart } from './events';
 export { getScope, updateScope } from './scope';
 export { useUnit } from './useUnit';
-export const mergeScope = getScope;
