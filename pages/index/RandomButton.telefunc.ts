c2d177c45382be9079fa605e162577d106c52962
Ivan Uzun <ivan@motorway.co.uk>
2024-10-04 06:31:36 +0100

    chore: telefunc await

--- pages/index/RandomButton.telefunc.ts
+++ pages/index/RandomButton.telefunc.ts
@@ -2,7 +2,13 @@ import { fork, serialize } from 'effector';
 
 import { service } from './model';
 
+const wait = (time: number) =>
+  new Promise((resolve) => {
+    setTimeout(() => resolve(true), time);
+  });
+
 export const onGenerateRandom = async () => {
+  await wait(2000);
   const scope = fork({
     values: [[service.$random, Math.round(Math.random() * 1000)]],
   });
