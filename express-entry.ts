7fb4d02926ffef4962ca6bf9dcf6c5677a0d049a
Ivan Uzun <ivan@motorway.co.uk>
2025-01-19 13:50:46 +0000

    chore: deps

--- express-entry.ts
+++ express-entry.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { exec } from 'node:child_process';
 import { dirname } from 'node:path';
 import { fileURLToPath } from 'node:url';
+import { createDevMiddleware } from 'vike/server';
 
 import { createHandler } from '@universal-middleware/express';
 
@@ -12,7 +13,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const root = __dirname;
 const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
-const hmrPort = process.env.HMR_PORT ? parseInt(process.env.HMR_PORT, 10) : 24678;
 
 export default (await startServer()) as unknown;
 
@@ -22,17 +22,8 @@ async function startServer() {
   if (process.env.NODE_ENV === 'production') {
     app.use(express.static(`${root}/dist/client`));
   } else {
-    // Instantiate Vite's development server and integrate its middleware to our server.
-    // ⚠️ We should instantiate it *only* in development. (It isn't needed in production
-    // and would unnecessarily bloat our server in production.)
-    const vite = await import('vite');
-    const viteDevMiddleware = (
-      await vite.createServer({
-        root,
-        server: { middlewareMode: true, hmr: { port: hmrPort } },
-      })
-    ).middlewares;
-    app.use(viteDevMiddleware);
+    const { devMiddleware } = await createDevMiddleware({ root });
+    app.use(devMiddleware);
   }
 
   app.post('/_telefunc', createHandler(telefuncHandler)());
