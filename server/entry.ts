03b43622007034e3e87a78c7a36097fe0cd99777
Ivan Uzun <ivan@motorway.co.uk>
2025-03-31 23:20:20 +0100

    chore: deps

--- server/entry.ts
+++ server/entry.ts
@@ -16,8 +16,6 @@ const __dirname = dirname(__filename);
 const root = `${__dirname}/..`;
 const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
 
-export default (await startServer()) as unknown;
-
 async function startServer() {
   const app = express();
 
@@ -65,3 +63,5 @@ async function startServer() {
 
   return app;
 }
+
+await startServer();
